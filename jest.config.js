f40325534438c7ce131c0e806f62d0cc2fe826d8
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2026-02-22 11:32:51 +0800

    jest: fixed config to skip the e2e test with jest.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-        '^obsidian$': '<rootDir>/__mocks__/obsidian.ts',
-    },
+	preset: "ts-jest",
+	testEnvironment: "node",
+	testPathIgnorePatterns: ["/node_modules/", "/tests/e2e/"],
+	moduleNameMapper: {
+		"^obsidian$": "<rootDir>/__mocks__/obsidian.ts",
+	},
 };
